61865b0d06faa6b7ea163ba8f632fea832e2d549
Marc Von Asken <vonasken@gmail.com>
2017-04-09 10:36:56 -0700

    first commit

---
+++ HelloWorld/app/app.js
@@ -0,0 +1,3 @@
+var applicationModule = require("application");
+applicationModule.start({ moduleName: "main-page" });
+import camera = require("camera");
---
+++ HelloWorld/platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,7 @@
+dependencies: /Users/VonAsken/HelloWorld/platforms/ios/internal/main.m \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/VonAsken/HelloWorld/platforms/ios/internal/TNSDebugging.h
