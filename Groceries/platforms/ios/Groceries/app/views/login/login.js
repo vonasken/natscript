14f0def14985848cc0ac9f6f2cdfea9e1e75a889
Marc Von Asken <vonasken@gmail.com>
2017-04-10 23:58:49 -0700

    backend link

--- Groceries/platforms/ios/Groceries/app/views/login/login.js
+++ Groceries/platforms/ios/Groceries/app/views/login/login.js
@@ -1,9 +1,6 @@
-var Observable = require("data/observable").Observable;
+var UserViewModel = require("../../shared/view-models/user-view-model");
 
-var user = new Observable({
-    email: "user@domain.com",
-    password: "password"
-});
+var user = new UserViewModel();
 
 var frameModule = require("ui/frame");
 
@@ -16,8 +13,7 @@ exports.loaded = function(args) {
 };
 
 exports.signIn = function() {
-    email = page.getViewById("email");
-    console.log(email.text);
+    user.login();
 };
 
 exports.register = function() {
