40f8be24f5cb4132b0d3bd84f9859dfc5f9715b8
Marc Von Asken <vonasken@gmail.com>
2017-04-11 00:19:44 -0700

    more login logic

--- Groceries/app/views/login/login.js
+++ Groceries/app/views/login/login.js
@@ -1,3 +1,5 @@
+var dialogsModule = require("ui/dialogs");
+
 var UserViewModel = require("../../shared/view-models/user-view-model");
 
 var user = new UserViewModel();
@@ -13,7 +15,18 @@ exports.loaded = function(args) {
 };
 
 exports.signIn = function() {
-    user.login();
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
 };
 
 exports.register = function() {
