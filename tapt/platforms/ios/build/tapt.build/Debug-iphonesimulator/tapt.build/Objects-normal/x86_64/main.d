4ccadc51d32250e8e855b6a55940c06568eb33d2
Marc Von Asken <vonasken@gmail.com>
2017-04-18 00:30:48 -0700

    timer set up

---
+++ tapt/platforms/ios/build/tapt.build/Debug-iphonesimulator/tapt.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/main.m \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/VonAsken/Desktop/SeyCali/Natscript/tapt/platforms/ios/internal/TNSDebugging.h
