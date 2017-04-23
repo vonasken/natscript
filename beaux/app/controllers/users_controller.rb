c449f5ae22eeed32dca0fc20668f82937006620e
Marc Von Asken <vonasken@gmail.com>
2017-04-22 19:48:51 -0700

    Show Template

--- beaux/app/controllers/users_controller.rb
+++ beaux/app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
-  before_action :autheticate_user!, only: [:show]
-
-  def show
-    @user = User.find(params[:id])
-    render :show
-  end
+def show
+  @user = User.find(params[:id])
+render :show
+end
 end
--- beaux/config/routes.rb
+++ beaux/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get '/' => 'home#index'
+
+
+
+  get '/users/:id', to: 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
