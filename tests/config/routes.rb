03b28d789e1322c0d01f4f521593ffab3bca89c0
Marc Von Asken <vonasken@gmail.com>
2017-04-25 21:18:01 -0700

    stripes

--- tests/config/routes.rb
+++ tests/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'welcome#index'
 
-  get '/payment', to: 'welcome#payment'
+  
+
+  resources :charges
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
