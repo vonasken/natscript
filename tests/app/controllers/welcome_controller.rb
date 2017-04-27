198a5e4a2162357c00f4cb90e788d2469ad52d53
Marc Von Asken <vonasken@gmail.com>
2017-04-26 22:09:54 -0700

    routes

--- tests/app/controllers/welcome_controller.rb
+++ tests/app/controllers/welcome_controller.rb
@@ -1,10 +1,17 @@
 class WelcomeController < ApplicationController
   def index
-    
+
   end
 
   def payment
 
   end
 
+  def about
+
+  end
+
+  def tips
+
+  end
 end
--- tests/config/routes.rb
+++ tests/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'welcome#index'
 
-  
+  get '/about' => 'welcome#about'
+
+  get '/tips' => 'welcome#tips'
+
+
 
   resources :charges
 
